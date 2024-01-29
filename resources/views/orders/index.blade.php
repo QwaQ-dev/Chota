f7d9711e11e3abf39a0200d4c251a8801bfafc9b
qwaqd <qwaq.dev@gmail.com>
2024-01-29 15:29:59 +0500

    Верстка orders

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -52,6 +52,20 @@
                 </div>
 
                 <div class="max-w-xl">
+                    <x-input-label for="type_work" :value="__('Выберите сырье со склада')" />
+                    <x-text-input id="typeworks" name="typeworks" type="text" class="mt-1 block w-full" list="typeworks"
+                                  required autocomplete="typeworks" />
+                    <datalist id="typeworks">
+                        <option value="Изготовление импланта">
+                        <option value="Изготовление протезов">
+                        <option value="Изготовление виниров">
+                        <option value="Изготовление циркониевых коронок">
+                        <option value="Изготовление керамических накладок">
+                        <option value="Другое">
+                    </datalist>
+                </div>
+
+                <div class="max-w-xl">
                     <x-input-label for="summ" :value="__('Specify a sum')" />
                     <x-text-input id="summ" name="summ" type="text" class="mt-1 block w-full" required
                                   autofocus autocomplete="summ" />
@@ -107,190 +121,69 @@
                         {{ __('navigation.task.active') }}</summary>
                     @foreach ($orders[0] as $order)
                         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-10">
-                            <div class="p-6 text-gray-900 dark:text-gray-100">
+                            <div class="p-6 text-gray-900 dark:text-gray-100 flex justify-around">
                                 <form method="post" action="{{ route('orders.update', $order->id) }}" class="mt-6 space-y-6">
                                     @csrf
                                     @method('patch')
-
                                     <div class="max-w-xl">
-                                        <label class="text-lg font-medium text-gray-900 dark:text-gray-100" for="order_id">
+                                        <label class="text-lg font-bold text-gray-900 dark:text-gray-100" for="order_id">
                                             Заказ
                                         </label>
                                         <span class="block mt-1">{{ $order->id }}</span>
                                     </div>
 
                                     <div class="max-w-xl">
-                                        <label class="text-lg font-medium text-gray-900 dark:text-gray-100" for="username">
+                                        <label class="text-lg font-bold text-gray-900 dark:text-gray-100" for="username">
                                             Клиент
                                         </label>
                                         <span class="block mt-1">{{ $order->username }}</span>
                                     </div>
 
                                     <div class="max-w-xl">
-                                        <label class="text-lg font-medium text-gray-900 dark:text-gray-100" for="typeworks">
+                                        <label class="text-lg font-bold text-gray-900 dark:text-gray-100" for="typeworks">
                                             Работа
                                         </label>
                                         <span class="block mt-1">{{ $order->typeworks }}</span>
                                     </div>
 
                                     <div class="max-w-xl">
-                                        <label class="text-lg font-medium text-gray-900 dark:text-gray-100" for="summ">
-                                            Сумма заказа
+                                        <label class="text-lg font-bold text-gray-900 dark:text-gray-100" for="summ">
+                                            Сырье со склада
                                         </label>
                                         <span class="block mt-1">{{ $order->summ }}</span>
                                     </div>
 
                                     <div class="max-w-xl">
-                                        <label class="text-lg font-medium text-gray-900 dark:text-gray-100" for="executor">
-                                            Исполнители
+                                        <label class="text-lg font-bold text-gray-900 dark:text-gray-100" for="summ">
+                                            Сумма заказа
                                         </label>
-                                        <span class="block mt-1">{{ $order->executor }}</span>
+                                        <span class="block mt-1">{{ $order->summ }}</span>
+                                    </div>
+                                </form>
+                                <form method="post" action="{{ route('orders.update', $order->id) }}" class="mt-6 space-y-6">
+                                    @csrf
+                                    @method('patch')
+                                    <div class="max-w-xl">
+                                        <label class="text-lg font-medium text-gray-900 dark:text-gray-100" for="order_id">
+                                            Ход работы
+                                        </label>
                                     </div>
 
-                                    <!-- Добавьте другие поля по необходимости -->
-
-                                    <div class="flex items-center gap-4">
-                                        <div class="flex justify-end mt-4">
-                                            <form action="{{ route('orders.completed', $order->id) }}" method="post">
-                                                @csrf
-                                                @method('patch')
-                                                <x-primary-button name="accept" value="{{ $order->id }}">
-                                                    {{ __('navigation.task.done') }}</x-primary-button>
-                                            </form>
-                                            <form action="{{ route('orders.update', $order->id) }}" method="post"
-                                                  class="ml-3">
-                                                @csrf
-                                                @method('patch')
-                                                <x-primary-button name="refuse" value="{{ $order->id }}">
-                                                    {{ __('navigation.task.assign') }}</x-primary-button>
-                                            </form>
-                                        </div>
+                                    <div class="max-w-xl">
+                                        <input type="checkbox"
+                                               class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-gray-900/20 bg-gray transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-700 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:scale-105 hover:before:opacity-0"
+                                               id="customStyle" disabled/>
+                                        <label class="text-lg font-medium text-gray-900 dark:text-gray-100 ml-2" for="order_id">
+                                            Имя
+                                        </label>
                                     </div>
                                 </form>
                             </div>
                         </div>
                     @endforeach
                 </details>
-                <details open>
-                    <summary
-                        class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-full">
-                        {{ __('navigation.task.wait') }}</summary>
-                    @if ($orders !== null && count($orders[1]) == 0)
-                        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-10">
-                            <div class="p-6 text-gray-900 dark:text-gray-100">
-                                <div class="flex justify-between">{{ __('navigation.task.information') }}</div>
-                                <div class="mt-4">{{ __('navigation.task.information_text') }}</div>
-                            </div>
-                        </div>
-                    @else
-                        @foreach ($orders[1] as $order)
-                            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-10">
-                                <div class="p-6 text-gray-900 dark:text-gray-100">
-                                    <div class="flex justify-between capitalize">
-                                        <div class="flex justify-between">
-                                            <span class="task__header_user">{{ $users[$order->user_id - 0]->name }}
-                                            </span>
-                                            <div class="gray line"></div>
-                                            <span class="task__header_cabinet">{{ $order->cabinet }}
-                                                {{ __('navigation.task.cabinet') }} </span>
-                                        </div>
-                                        <div class="flex justify-end">
-                                            <span>
-                                                {{ Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $order->created_at)->locale(App::currentLocale())->isoFormat('dddd, D MMMM H:mm') }}
-                                            </span>
-                                        </div>
-                                    </div>
-                                    <div class="mt-4">
-                                        <div class="task__title">{{ $order->username }}</div>
-                                        <div class="task__description">{{ $order->typeworks }}</div>
-                                        @if (Auth::user()->isAdmin() && $order->status_id == 0)
-                                            <div class="flex justify-between mt-6">
-                                                <form action="{{ route('orders.destroy', $order->id) }}" method="post">
-                                                    @csrf
-                                                    @method('delete')
-                                                    <x-accept-button>
-                                                        {{ __('Delete') }}
-                                                    </x-accept-button>
-                                                </form>
 
-                                                <form action="{{ route('orders.update', $order->id) }}" method="post">
-                                                    @csrf
-                                                    @method('patch')
-                                                    <x-select name="performer_id">
-                                                        @foreach ($workers as $worker)
-                                                            <option value="{{ $worker['user_id'] }}">
-                                                                {{ $worker['name'] }}</option>
-                                                        @endforeach
-                                                    </x-select>
-                                                    <x-primary-button name="accept" value="{{ $order->id }}"
-                                                                      class="ml-3">
-                                                        {{ __('navigation.task.assign') }}
-                                                    </x-primary-button>
-                                                </form>
-                                            </div>
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
-                        @endforeach
-                    @endif
-                </details>
-                @if (Auth::user()->isAdmin())
-                @endif
-                @if ($orders !== null && is_countable($orders[1]) && count($orders[1]) == 0)
-                    <details>
-                        <summary
-                            class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-full">
-                            {{ __('navigation.task.completed') }}</summary>
-                        <div class="mt-3 mb-3 text-gray-900 dark:text-gray-100">
-                            <form action="{{ route('task.export') }}" method="post">
-                                @csrf
-                                <!-- @method('patch') -->
-                                <label for="start">{{ __('navigation.task.from') }}:</label>
 
-                                <input type="date" id="start"
-                                       class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
-                                       name="from" value="{{ date('Y-m-d', strtotime('-1 month')) }}"
-                                       min="2023-01-01" max="2033-12-31" style="border-radius: 0.5em;">
-
-                                <label class="ml-3" for="end">{{ __('navigation.task.to') }}:</label>
-
-                                <input type="date" id="end"
-                                       class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
-                                       name="to" value="{{ date('Y-m-d') }}" min="2023-01-01" max="2033-12-31"
-                                       style="border-radius: 0.5em;">
-
-                                <x-primary-button class="ml-3">{{ __('navigation.task.export') }}</x-primary-button>
-                            </form>
-                        </div>
-                        @foreach ($orders[2] as $order)
-                            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-10">
-                                <div class="p-6 text-gray-900 dark:text-gray-100">
-                                    <div class="flex justify-between capitalize">
-                                        <div class="flex justify-between">
-                                            <span>{{ $users[$order->user_id - 1]->name }}</span>
-                                            <div class="gray line"></div>
-                                            <span>{{ $order->cabinet }}
-                                                {{ __('navigation.task.cabinet') }}</span>
-                                        </div>
-                                        <div class="flex justify-end">
-                                            <span>
-                                                {{ Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $task->updated_at)->locale(App::currentLocale())->isoFormat('dddd, D MMMM H:mm') }}
-                                            </span>
-                                        </div>
-                                    </div>
-                                    <div class="mt-4">
-                                        <span>{{ $order->title }}</span>
-                                        <span>{{ $order->description }}</span>
-                                        <span
-                                            class="flex justify-end mt-6 mr-2">{{ __('navigation.task.performer') }}:
-                                            {{ $workers[array_search($order->performer_id, array_column($workers, 'user_id'))]['name'] }}</span>
-                                    </div>
-                                </div>
-                            </div>
-                        @endforeach
-                    </details>
-                @endif
             </div>
         </div>
     @endif
